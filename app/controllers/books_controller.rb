497687cac847406422c717635bb1f579b0e46ffc
jason.kim7288 <jason.kim7288@gmail.com>
2020-03-20 18:20:55 +1000

    Generate controllers

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,22 @@
+class BooksController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destory
+  end
+
+  def index
+  end
+
+  def show
+  end
+end
